ee34f9d20be8177454f69d2807ed7b884ac517d5
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 13:07:59 +0800

    First Print

--- Current+Previous.py
+++ Current+Previous.py
@@ -1,4 +1,5 @@
 # Print "Printing current and previous number sum in a range(10)"
+print("Printing current and previous number sum in a range(10)")
 # Looping from 1 to 10
 # Addition Function
 # Print "Current Number 0 Previous Number 0 Sum: 0"
