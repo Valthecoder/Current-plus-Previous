06aa297b51052cde8a9fefb699e5099b41d41ec9
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 13:27:54 +0800

    Looping and Addition

--- Current+Previous.py
+++ Current+Previous.py
@@ -1,6 +1,8 @@
 # Print "Printing current and previous number sum in a range(10)"
 print("Printing current and previous number sum in a range(10)")
 # Looping from 1 to 10
-# Addition Function
+p_num = 0
+for r_num in range(1, 11):
+    # Addition Function
+    cp_num = p_num + r_num
 # Print "Current Number 0 Previous Number 0 Sum: 0"
-
